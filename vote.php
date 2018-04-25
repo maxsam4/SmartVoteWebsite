65af3dbc6f6f64574b59e2a3a9c1398815e1a307
Mudit <guptamudit@ymail.com>
2018-04-25 19:24:29 +0530

    vote

--- vote.php
+++ vote.php
@@ -8,7 +8,20 @@ $user_id = $_SESSION['user_session'];
 $stmt = $DB_con->prepare("SELECT * FROM voters WHERE id=:user_id");
 $stmt->execute(array(":user_id"=>$user_id));
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-//TODO cast vote
+
+if(isset($_POST['congress']))
+{
+    $ex = shell_exec('./simplewallet --voter ' + $userRow['username'] + ' --password abc --party VwNZz3XdQwc6W84a66Pf86YwiBCLdVr21VnvQGvRRXCGZqk8zkUhV35JjKkp4hs9KtG8FpVsRsHVDTzcCuJDpERJ2caXfFu88');
+    $stmt = $DB_con->prepare("UPDATE voters SET votedfor = 'Congress' WHERE id=:user_id");
+    $stmt->execute(array(":user_id"=>$user_id));
+}
+if(isset($_POST['bjp']))
+{
+    $ex = shell_exec('./simplewallet --voter ' + $userRow['username'] + ' --password abc --party VwMikWmwBtVfFvzui3Zu7FNqnL6eiPFm18dWtkF8JzdJaJ7xLT34QXZh32DYbCa9NzdTQUsUWQwx6eYMp2S1S13c2noeZgdZ9');
+    $stmt = $DB_con->prepare("UPDATE voters SET votedfor = 'BJP' WHERE id=:user_id");
+    $stmt->execute(array(":user_id"=>$user_id));
+}
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -30,8 +43,24 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
     </div>
 </div>
 <div class="content">
-    welcome : <?php print($userRow['username']); ?>
-
+<div class="form-container">
+    welcome : <?php print($userRow['username']);?> </br>
+    <?php if (isset($userRow['votedfor']) && !empty($userRow['votedfor'])){
+       echo '<h2> You have already voted for ' + $userRow['votedfor'] + '</h2>';
+    }else{?>
+    <form method="post">
+        <h2>Choose your candidate</h2><hr />
+        <div class="clearfix"></div><hr />
+        <button type="submit" name="congress" class="btn btn-block btn-primary">
+            <i class="glyphicon glyphicon-log-in"></i>&nbsp;Congress
+        </button>
+        <br /><br />
+        <button type="submit" name="bjp" class="btn btn-block btn-primary">
+            <i class="glyphicon glyphicon-log-in"></i>&nbsp;BJP
+        </button>
+    </form>
+    <?}?>
+</div>
 </div>
 </body>
 </html>
